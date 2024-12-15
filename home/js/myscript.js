b3150d49966fbcb61abf9b6056df9393cd15c12e
apcmmbalidio <mmbalidio@student.apc.edu.ph>
2024-12-15 21:14:46 +0800

    initial commit

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,7 +1,3 @@
-function updateParagraph() {
-    try {
-        document.getElementById("demo").innerHTML = "Paragraph changed.";
-    } catch (error) {
-        console.error("Error updating paragraph: ", error);
-    }
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
 }

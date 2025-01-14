6e80f2cd50d48b04f979020d2d38b830c794445d
apcmmbalidio <mmbalidio@student.apc.edu.ph>
2025-01-14 08:57:31 +0800

    add vue

--- sb/js/app.js
+++ sb/js/app.js
@@ -1,21 +1,29 @@
 new Vue({
-    el: '#app',
+    el: 'body',
     data: {
-        newComment: {
-            name: '',
-            message: ''
-        },
-        comments: []
+      newComment: {
+        name: '',
+        message: ''
+      },
+      comments: []
     },
     methods: {
-        addComment() {
-            const timestamp = new Date().toLocaleString();
-            this.comments.push({
-                ...this.newComment,
-                timestamp
-            });
-            this.newComment.name = '';
-            this.newComment.message = '';
+      addComment: function() {
+        if (this.newComment.name && this.newComment.message) {
+          const timestamp = new Date().toLocaleString();
+          const comment = {
+            name: this.newComment.name,
+            message: this.newComment.message,
+            timestamp: timestamp
+          };
+          
+          this.comments.push(comment); 
+          this.newComment.name = '';  
+          this.newComment.message = ''; 
+        } else {
+          alert('Please fill in both the name and the comment!');
         }
+      }
     }
-});
+  });
+  

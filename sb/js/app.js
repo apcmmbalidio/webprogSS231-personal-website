35f19af216ae0d60b6206c4fee69fc2bb1309322
apcmmbalidio <mmbalidio@student.apc.edu.ph>
2025-01-14 09:03:26 +0800

    comments

--- sb/js/app.js
+++ sb/js/app.js
@@ -1,29 +1,30 @@
 new Vue({
-    el: 'body',
+    el: '#app',
     data: {
-      newComment: {
-        name: '',
-        message: ''
-      },
-      comments: []
+        newComment: {
+            name: '',
+            message: ''
+        },
+        comments: []
     },
     methods: {
-      addComment: function() {
-        if (this.newComment.name && this.newComment.message) {
-          const timestamp = new Date().toLocaleString();
-          const comment = {
-            name: this.newComment.name,
-            message: this.newComment.message,
-            timestamp: timestamp
-          };
-          
-          this.comments.push(comment); 
-          this.newComment.name = '';  
-          this.newComment.message = ''; 
-        } else {
-          alert('Please fill in both the name and the comment!');
+        addComment: function() {
+            if (this.newComment.name && this.newComment.message) {
+                const timestamp = new Date().toLocaleString();
+                const comment = {
+                    name: this.newComment.name,
+                    message: this.newComment.message,
+                    timestamp: timestamp
+                };
+
+                this.comments.push(comment); 
+
+                // Clear the form fields after posting the comment
+                this.newComment.name = '';  
+                this.newComment.message = ''; 
+            } else {
+                alert('Please fill in both the name and the comment!');
+            }
         }
-      }
     }
-  });
-  
+});
